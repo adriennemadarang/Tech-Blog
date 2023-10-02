3d949ec74d3254e8a6f409a94713175fd917e011
adriennemadarang <adriennemadarang@icloud.com>
2023-10-02 14:06:22 -0700

    dashboard stuff

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboardRoutes');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', apiRoutes);
+router.use('/', dashboardRoutes);
+router.use('/', homeRoutes);
+
+
+
+
+
+module.exports = router;
